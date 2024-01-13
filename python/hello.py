7b292b8caf89d34488be6f1bd5ee424b05a3dee7
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2024-01-13 13:08:18 -0500

    Redid Python and Ruby

---
+++ python/hello.py
@@ -0,0 +1,5 @@
+import datetime
+
+current_date = datetime.datetime.now().strftime("%d/%m/%Y")
+print ("Python says, Hello ASL!")
+print (current_date)
---
+++ ruby/hello.rb
@@ -0,0 +1,4 @@
+current_date = Time.now.strftime("%d/%m/%Y")
+
+puts "Ruby says, Hello ASL!"
+puts current_date
